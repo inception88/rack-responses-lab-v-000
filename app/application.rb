a139f07db108c9df98fd48a52bc1123a5711149e
Benjamin Jones <bljones36@gmail.com>
2018-11-17 17:06:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
 
       resp.write "#{Time.now}/n"
 
-      if Time.now < 12
+      if Time.now.to_i < 12
         resp.write "Good Morning!"
       else
         resp.write "Good Afternoon!"
