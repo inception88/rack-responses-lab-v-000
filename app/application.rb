e878b7ee452655833043bbb0b11fa0fcf522a819
Benjamin Jones <bljones36@gmail.com>
2018-11-17 17:06:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,12 +2,10 @@ class Application
 
   def call(env)
   resp = Rack::Response.new
-
-      time = Kernel.rand(1..10)
-
+  
       resp.write "#{time}/n"
 
-      if time < 12
+      if Time.now < 12
         resp.write "Good Morning!"
       else
         resp.write "Good Afternoon!"
